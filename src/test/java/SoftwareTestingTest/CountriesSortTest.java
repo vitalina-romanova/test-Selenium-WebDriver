87a079af73f0fdd24e66a5a070085bfc278724d8
Vitalina <vitalinaromashka@mail.ru>
2021-04-14 22:44:50 +0300

    sortCountriesTest

--- src/test/java/SoftwareTestingTest/CountriesSortTest.java
+++ src/test/java/SoftwareTestingTest/CountriesSortTest.java
@@ -1,30 +1,54 @@
 package SoftwareTestingTest;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class CountriesSortTest  extends BaseTest {
 
+
+    private void assertABC(List<String> strings){
+        for (int i =1; i<strings.size(); i++ ){
+            String current = strings.get(i);
+            String previous = strings.get(i-1);
+            Assert.assertTrue(current.compareTo(previous) > 0);
+        }
+    }
+
     @Test
     public void sortCountriesTest() {
         openCountriesPage();
-        List<WebElement> listCountries = driver.findElements(By.cssSelector("tr.row a[href*=country_code]"));
+        List<WebElement> rowCountries = driver.findElements(By.cssSelector("tr.row"));
         List<String> allCountries = new ArrayList<String>();
-        for (WebElement row : listCountries){
-            String nameCountry = row.getAttribute("innerText");
-            if (!nameCountry.equals(""))
-                allCountries.add(nameCountry);
-        }
-        for (int i =1; i<allCountries.size(); i++ ){
-            String current = allCountries.get(i);
-            String previous = allCountries.get(i-1);
-            if (current.compareTo(previous) < 0){
-                throw new RuntimeException();
+        int size = rowCountries.size();
+        for (int i = 0; i < size; i++) {
+            WebElement countryElement = rowCountries.get(i).findElement(By.cssSelector("td:nth-child(5) a"));
+            String nameCountry = countryElement.getAttribute("innerText");
+            allCountries.add(nameCountry);
+            WebElement zoneElement = rowCountries.get(i).findElement(By.cssSelector("td:nth-child(6)"));
+            String count = zoneElement.getAttribute("innerText");
+
+            if (!count.equals("0")) {
+                countryElement.click();
+                wait.until(ExpectedConditions.titleIs("Edit Country | My Store"));
+                List<WebElement> zoneElements = driver.findElements(By.cssSelector("input[name*='[name]']:not([type='text'])"));
+                List<String> allZones = new ArrayList<String>();
+                for (WebElement zone : zoneElements) {
+                    String nameZone = zone.getAttribute("value");
+                    allZones.add(nameZone);
+                }
+                assertABC(allZones);
+                driver.navigate().back();
+                rowCountries = driver.findElements(By.cssSelector("tr.row"));
+                System.out.println(allZones);
             }
         }
+        assertABC(allCountries);
+        System.out.println(allCountries);
     }
 }
