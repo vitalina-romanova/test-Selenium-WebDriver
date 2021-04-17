f2f62a6a63585fc2f5ea1dab147338890b0154d1
Vitalina <vitalinaromashka@mail.ru>
2021-04-18 00:03:55 +0300

    UserRegistrationTest

--- src/test/java/SoftwareTestingTest/UserRegistrationTest.java
+++ src/test/java/SoftwareTestingTest/UserRegistrationTest.java
@@ -10,22 +10,22 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class UserRegistrationTest extends BaseTest{
 
+    private final String TAX_ID = "1234567890";
+    private final String COMPANY = "Test Company";
+    private final String FIRST_NAME = "Julius";
+    private final String LAST_NAME = "Meinl";
+    private final String ADDRESS = "200 W 33rd St, Austin, TX 78705, США";
+    private final String POSTCODE = getRandomNumber();
+    private final String CITY = "Austin";
+    private final String COUNTRY = "United States";
+    private final String STATE = "Texas";
+    private final String EMAIL = getRandomString(8) + "@test.ru";
+    private final String PHONE = "+1111111111";
+    private final String PASSWORD = "1q2w3e4r";
+
     public static String getRandomNumber() { return RandomStringUtils.randomNumeric(5); }
 
     public static String getRandomString(int length) { return RandomStringUtils.randomAlphabetic(length); }
-
-        private final String taxId = "1234567890";
-        private final String company = "Test Company";
-        private final String firstName = "Julius";
-        private final String lastName = "Meinl";
-        private final String address = "200 W 33rd St, Austin, TX 78705, США";
-        private final String postcode = getRandomNumber();
-        private final String city = "Austin";
-        private final String country = "United States";
-        private final String state = "Texas";
-        private final String email = getRandomString(8) + "@test.ru";
-        private final String phone = "+1111111111";
-        private final String password = "1q2w3e4r";
 
     @Test
     public void registrationUser() throws InterruptedException {
@@ -36,7 +36,7 @@ public class UserRegistrationTest extends BaseTest{
 
         //выбор страны
         driver.findElement(By.cssSelector("span[class*='select2']")).click();
-        driver.findElement(By.cssSelector("input[class*='search__field']")).sendKeys(country + Keys.ENTER);
+        driver.findElement(By.cssSelector("input[class*='search__field']")).sendKeys(COUNTRY + Keys.ENTER);
         Thread.sleep(500);
 
         //выбор штата
@@ -47,19 +47,19 @@ public class UserRegistrationTest extends BaseTest{
         //запоолнение формы
         Actions action = new Actions(driver);
         action.click()
-                .sendKeys(driver.findElement(By.name("tax_id")), taxId).sendKeys(Keys.TAB)
-                .sendKeys(company).sendKeys(Keys.TAB)
-                .sendKeys(firstName).sendKeys(Keys.TAB)
-                .sendKeys(lastName).sendKeys(Keys.TAB)
-                .sendKeys(address).sendKeys(Keys.TAB).sendKeys(Keys.TAB)
-                .sendKeys(postcode).sendKeys(Keys.TAB)
-                .sendKeys(city).sendKeys(Keys.TAB)
-                .sendKeys(country).sendKeys(Keys.TAB)
-                .sendKeys(state).sendKeys(Keys.TAB)
-                .sendKeys(driver.findElement(By.cssSelector("input[name='email']")), email).sendKeys(Keys.TAB)
-                .sendKeys(phone).sendKeys(Keys.TAB)
-                .sendKeys(driver.findElement(By.cssSelector("input[name='password']")), password).sendKeys(Keys.TAB)
-                .sendKeys(driver.findElement(By.cssSelector("input[name='confirmed_password']")), password)
+                .sendKeys(driver.findElement(By.name("tax_id")), TAX_ID).sendKeys(Keys.TAB)
+                .sendKeys(COMPANY).sendKeys(Keys.TAB)
+                .sendKeys(FIRST_NAME).sendKeys(Keys.TAB)
+                .sendKeys(LAST_NAME).sendKeys(Keys.TAB)
+                .sendKeys(ADDRESS).sendKeys(Keys.TAB).sendKeys(Keys.TAB)
+                .sendKeys(POSTCODE).sendKeys(Keys.TAB)
+                .sendKeys(CITY).sendKeys(Keys.TAB)
+                .sendKeys(COUNTRY).sendKeys(Keys.TAB)
+                .sendKeys(STATE).sendKeys(Keys.TAB)
+                .sendKeys(driver.findElement(By.cssSelector("input[name='email']")), EMAIL).sendKeys(Keys.TAB)
+                .sendKeys(PHONE).sendKeys(Keys.TAB)
+                .sendKeys(driver.findElement(By.cssSelector("input[name='password']")), PASSWORD).sendKeys(Keys.TAB)
+                .sendKeys(driver.findElement(By.cssSelector("input[name='confirmed_password']")), PASSWORD)
                 .perform();
 
         driver.findElement(By.cssSelector("button[name='create_account']")).click();
@@ -71,8 +71,8 @@ public class UserRegistrationTest extends BaseTest{
         Thread.sleep(500);
 
         //логин
-        driver.findElement(By.cssSelector("input[name='email']")).sendKeys(email);
-        driver.findElement(By.cssSelector("input[name='password']")).sendKeys(password);
+        driver.findElement(By.cssSelector("input[name='email']")).sendKeys(EMAIL);
+        driver.findElement(By.cssSelector("input[name='password']")).sendKeys(PASSWORD);
         driver.findElement(By.cssSelector("button[name='login']")).click();
         Thread.sleep(500);
 
