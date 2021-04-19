76b52ef107b0605755bbec165c039d81541e0dd7
Vitalina <vitalinaromashka@mail.ru>
2021-04-19 17:43:00 +0300

    refactor YellowDuckPriseTest

--- src/test/java/SoftwareTestingTest/YellowDuckPriseTest.java
+++ src/test/java/SoftwareTestingTest/YellowDuckPriseTest.java
@@ -36,20 +36,22 @@ public class YellowDuckPriseTest extends BaseTest {
         List<String> rgbaRegularPrice = Arrays.asList(colorRegularPrice.substring( 5, 19)
                 .replaceAll("\\s", "").split(","));
         String tegRegularPrice = regularPriceElement.getAttribute("tagName");
-        String sizeRegularPrice = regularPriceElement.getCssValue("font-size");
+        double sizeRegularPrice = Double.parseDouble(regularPriceElement.getCssValue("font-size")
+                .replaceAll("px", ""));
 
         WebElement campaignPriceElement = driver.findElement(By.cssSelector("div#box-campaigns .campaign-price"));
         String colorCampaignPrice = campaignPriceElement.getCssValue("color");
         List<String> rgbaCampaignPrice = Arrays.asList(colorCampaignPrice.substring( 5, 14)
                 .replaceAll("\\s", "").split(","));
         String tegCampaignPrice = campaignPriceElement.getAttribute("tagName");
-        String sizeCampaignPrice = campaignPriceElement.getCssValue("font-size");
+        double sizeCampaignPrice = Double.parseDouble(campaignPriceElement.getCssValue("font-size")
+                .replaceAll("px", ""));
 
         Assert.assertEquals(rgbaRegularPrice.get(0),rgbaRegularPrice.get(1),rgbaRegularPrice.get(2));
         Assert.assertEquals(tegRegularPrice,"S");
         Assert.assertEquals(rgbaCampaignPrice.get(1),rgbaCampaignPrice.get(2), "0");
         Assert.assertEquals(tegCampaignPrice,"STRONG");
-        Assert.assertTrue(sizeCampaignPrice.compareTo(sizeRegularPrice) > 0);
+        Assert.assertTrue(sizeCampaignPrice > sizeRegularPrice);
     }
 
     @Test
@@ -64,19 +66,21 @@ public class YellowDuckPriseTest extends BaseTest {
         String tegRegular = regularElement.getAttribute("tagName");
         List<String> rgbaRegular = Arrays.asList(colorRegular.substring( 5, 19)
                 .replaceAll("\\s", "").split(","));
-        String sizeRegular = regularElement.getCssValue("font-size");
+        double sizeRegular = Double.parseDouble(regularElement.getCssValue("font-size")
+                .replaceAll("px", ""));
 
         WebElement campaignElement = driver.findElement(By.cssSelector("div.price-wrapper .campaign-price"));
         String colorCampaign = campaignElement.getCssValue("color");
         String tegCampaign = campaignElement.getAttribute("tagName");
         List<String> rgbaCampaign = Arrays.asList(colorCampaign.substring( 5, 14)
                 .replaceAll("\\s", "").split(","));
-        String sizeCampaign = campaignElement.getCssValue("font-size");
+        double sizeCampaign = Double.parseDouble(campaignElement.getCssValue("font-size")
+                .replaceAll("px", ""));
 
         Assert.assertEquals(rgbaRegular.get(0),rgbaRegular.get(1),rgbaRegular.get(2));
         Assert.assertEquals(tegRegular,"S");
         Assert.assertEquals(rgbaCampaign.get(1),rgbaCampaign.get(2), "0");
         Assert.assertEquals(tegCampaign,"STRONG");
-        Assert.assertTrue(sizeCampaign.compareTo(sizeRegular) > 0);
+        Assert.assertTrue(sizeCampaign > sizeRegular);
     }
 }
